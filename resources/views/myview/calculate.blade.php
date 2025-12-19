4dbbbe20cd1df6bba108c06343a00ebe23fb1368
Som-Haji <noeznolife12@gmail.com>
2025-12-19 17:26:06 +0700

    Lab4 unfinished
    -Html101_view undefined and still not done

---
+++ resources/views/myview/calculate.blade.php
@@ -0,0 +1,15 @@
+@extends('template.default')
+
+@section('title', 'myview')
+@section('content')
+<ul>
+    <?php
+    // $num = 2 ;
+    for ($i=1 ; $i<=12 ; $i++){
+    ?>
+        <li>
+            {{$num}} x {{$i}} = {{$num * $i}};
+        </li>
+    <?php } ?>
+</ul>
+@endsection
